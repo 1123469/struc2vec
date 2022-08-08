1f46f7cad5f1be91c2d4f11ce3e7b32f1c0af1d8
lizihan <compact_set@foxmail.com>
2022-08-08 08:09:13 +0800

    update README.md in Week 2

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # struc2vec
+
+## Week2：
+
+利用Barbell Graph,b(1,1)对计算Structure Similarity代码进行测试
+
+- (1,1)-Barbell图
+
+  ![week2_test_barbell](pic/week2_test_barbell.png)
+
+  
+
+- 输入图adjacency matrix
+
+  ![](pic/week2_test_matrix.png)
+
+- 输出结果
+
+  ![](pic/week2_test_result.png)
+
+- 
+
--- week2/MainEnter.java
+++ week2/MainEnter.java
@@ -5,17 +5,25 @@ public class MainEnter {
     // By computing the structure similarity of Barbell graph b(2,1)
     public static final int INF = Integer.MAX_VALUE;
     public static void main(String[] args){
+//        Integer[][] graph = {
+//                {0,1,INF,INF,INF},
+//                {1,0,1,INF,INF},
+//                {INF,1,0,1,INF},
+//                {INF,INF,1,0,1},
+//                {INF,INF,INF,1,0},
+//        };
         Integer[][] graph = {
-                {0,1,INF,INF,INF},
-                {1,0,1,INF,INF},
-                {INF,1,0,1,INF},
-                {INF,INF,1,0,1},
-                {INF,INF,INF,1,0},
+                {0,1,INF},
+                {1,0,1},
+                {INF,1,0},
+
         };
         StructureSimilarity strSimi = new StructureSimilarity(graph);
         for(int i=0;i<=strSimi.diam;i++)
             strSimi.computeLayerK(i);
+        System.out.println("hopCountResult:");
         System.out.println(Arrays.deepToString(strSimi.hopCountResult));
+        System.out.println("structSimi:");
         System.out.println(Arrays.deepToString(strSimi.structSimi));
 //        System.out.println(Arrays.deepToString(strSimi.hopCountResult));
 //        System.out.println(Arrays.deepToString(strSimi.getHopRingK(0,1)));
